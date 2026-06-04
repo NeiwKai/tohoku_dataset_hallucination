af23a8a3823c95ed1eb3d84c75e2b754f00fa53b
Suppanat Timngarm <111691144+NeiwKai@users.noreply.github.com>
2026-06-04 21:06:08 +0900

    add convert for dataset_logging.csv

--- converter.py
+++ converter.py
@@ -1,18 +1,28 @@
 import pandas as pd
 
 # Read CSV
-df = pd.read_csv("conference_paper.csv")
+df_conference = pd.read_csv("conference_paper.csv")
+df_dataset = pd.read_csv("dataset_logging.csv")
 
 # Split into title and year
-df[["title", "year"]] = df["conference_source"].str.rsplit(
+df_conference[["title", "year"]] = df_conference["conference_source"].str.rsplit(
+    ",", n=1, expand=True
+)
+df_dataset[["title", "year"]] = df_dataset["conference_source"].str.rsplit(
     ",", n=1, expand=True
 )
 
 # Remove leading/trailing whitespace
-df["title"] = df["title"].str.strip()
-df["year"] = df["year"].str.strip()
+df_conference["title"] = df_conference["title"].str.strip()
+df_dataset["year"] = df_dataset["year"].str.strip()
 
-df = df.drop(columns=["conference_source"])
+df_dataset["title"] = df_dataset["title"].str.strip()
+df_dataset["year"] = df_dataset["year"].str.strip()
+
+
+df_conference = df_conference.drop(columns=["conference_source"])
+df_dataset = df_dataset.drop(columns=["conference_source"])
 
 # Save result
-df.to_csv("conference_paper.csv", index=False, quoting=1)
+df_conference.to_csv("conference_paper.csv", index=False, quoting=1)
+df_dataset.to_csv("dataset_logging.csv", index=False, quoting=1)
